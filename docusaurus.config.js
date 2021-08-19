1d02a6b7ef4f4501bc206d7a0f034dd89baca116
zodea <zd929560638@gmail.com>
2021-08-19 13:42:44 +0800

    feat: 初步添加vite1的内容
    feat: 添加vite搭建的代码格式化部分内容

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,6 +3,10 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
+  i18n: {
+    defaultLocale: 'zh-cn',
+    locales: ['en', 'zh-cn'],
+  },
   title: 'Zodea小屋',
   tagline: '',
   url: 'https://github.com/zodea.github.io',
@@ -21,6 +25,7 @@ module.exports = {
       },
       items: [
         { to: '/', label: 'Blog', position: 'left' },
+        { to: '/tags', label: '标签', position: 'left' },
         {
           href: 'https://github.com/zodea',
           label: 'GitHub',
@@ -51,6 +56,17 @@ module.exports = {
       theme: lightCodeTheme,
       darkTheme: darkCodeTheme,
     },
+    // algolia: {
+    //   apiKey: 'YOUR_API_KEY',
+    //   indexName: 'YOUR_INDEX_NAME',
+    //   // Optional: see doc section below
+    //   contextualSearch: true,
+    //   // Optional: see doc section below
+    //   appId: 'YOUR_APP_ID',
+    //   // Optional: Algolia search parameters
+    //   searchParameters: {},
+    //   //... other Algolia params
+    // },
   },
   presets: [
     [
@@ -61,19 +77,12 @@ module.exports = {
           path: './blog',
           routeBasePath: '/',
           showReadingTime: true,
+          blogTitle: 'Zodea小屋',
+          blogDescription: '发掘新兴技术',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-      },
-    ],
-  ],
-  plugins: [
-    [
-      require.resolve('@easyops-cn/docusaurus-search-local'),
-      {
-        hashed: true,
-        language: ['en', 'zh'],
       },
     ],
   ],
